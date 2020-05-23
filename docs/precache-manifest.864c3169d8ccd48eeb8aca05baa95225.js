62f48924c7369965781d88234a2dba330ac03e56
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-05-23 20:05:03 +0000

    Updates

---
+++ docs/precache-manifest.864c3169d8ccd48eeb8aca05baa95225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67ebb77f245dfb60acbd5e3048eae56",
+    "url": "/react-vocabulary-builder/index.html"
+  },
+  {
+    "revision": "07e63f6bab7f197b764f",
+    "url": "/react-vocabulary-builder/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "ffaf35ba5fa45cee6fd1",
+    "url": "/react-vocabulary-builder/static/js/2.5bdb0ca8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-vocabulary-builder/static/js/2.5bdb0ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e63f6bab7f197b764f",
+    "url": "/react-vocabulary-builder/static/js/main.44e9ba50.chunk.js"
+  },
+  {
+    "revision": "be3af73703011c60609f",
+    "url": "/react-vocabulary-builder/static/js/runtime-main.31a99c34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-vocabulary-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
