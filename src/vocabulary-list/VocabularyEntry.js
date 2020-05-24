64620bc292f39ecdf21ed04df54f4a544ebc094e
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2020-05-24 13:40:57 +0200

    Add entry component with remove button

---
+++ src/vocabulary-list/VocabularyEntry.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function VocabularyEntry(props) {
+  const { word, translation } = props.entry;
+
+  return (
+    <ul className="list-group list-group-horizontal-xl">
+      <li className="list-group-item">{word}</li>
+      <li className="list-group-item">{translation}</li>
+    </ul>
+  )
+}
--- src/vocabulary-list/VocabularyList.js
+++ src/vocabulary-list/VocabularyList.js
@@ -1,14 +1,32 @@
 import React from "react";
 import VocabularyAddEntry from "./VocabularyAddEntry";
+import { useVocabularyStore } from "../persistence/persistence";
+import VocabularyEntry from "./VocabularyEntry";
+import RemoveButton from "../misc/RemoveButton";
 
 export default function VocabularyList() {
+  const { list, appendList, removeFromList } = useVocabularyStore();
   const addEntry = (word, translation) => {
-    console.log(word, translation);
+    appendList({
+      word,
+      translation
+    });
   };
 
+  const entries = list.map((entry, idx) =>
+    <li className="list-group-item d-flex" key={entry.word + entry.translation}>
+      <VocabularyEntry entry={entry}></VocabularyEntry>
+      <RemoveButton onClick={() => removeFromList(idx)}></RemoveButton>
+    </li>
+    // relying on the order in the array, might be risky if one does reverse before mapping
+    // but it's simple
+  ).reverse();
   return (
     <div>
       <VocabularyAddEntry onAddEntry={addEntry}></VocabularyAddEntry>
-    </div>
+      <ul className="list-group">
+        {entries}
+      </ul>
+    </div >
   );
 }
